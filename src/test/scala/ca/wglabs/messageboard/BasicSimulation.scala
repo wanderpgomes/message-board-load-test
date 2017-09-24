6c95ad8dd96f485af5207b86028457e98fcaa6cb
wander <wander.gomes@gmail.com>
2017-09-24 19:39:44 -0400

    Add Basic Simulation with three scenarios.

--- src/test/scala/ca/wglabs/messageboard/BasicSimulation.scala
+++ src/test/scala/ca/wglabs/messageboard/BasicSimulation.scala
@@ -9,50 +9,24 @@ class BasicSimulation extends Simulation {
 
   val httpConf = http
     .baseURL("https://localhost:8443") // Here is the root for all relative URLs
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
-    .acceptEncodingHeader("gzip, deflate")
-    .acceptLanguageHeader("en-US,en;q=0.5")
-    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
-  /*val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
-    .exec(http("request_1")
-      .get("/"))
-    .pause(7) // Note that Gatling has recorder real time pauses
-    .exec(http("request_2")
-      .get("/computers?f=macbook"))
-    .pause(2)
-    .exec(http("request_3")
-      .get("/computers/6"))
-    .pause(3)
-    .exec(http("request_4")
-      .get("/"))
-    .pause(2)
-    .exec(http("request_5")
-      .get("/computers?p=1"))
-    .pause(670 milliseconds)
-    .exec(http("request_6")
-      .get("/computers?p=2"))
-    .pause(629 milliseconds)
-    .exec(http("request_7")
-      .get("/computers?p=3"))
-    .pause(734 milliseconds)
-    .exec(http("request_8")
-      .get("/computers?p=4"))
-    .pause(5)
-    .exec(http("request_9")
-      .get("/computers/new"))
-    .pause(1)
-    .exec(http("request_10") // Here's an example of a POST request
-      .post("/computers")
-      .formParam("""name""", """Beautiful Computer""") // Note the triple double quotes: used in Scala for protecting a whole chain of characters (no need for backslash)
-      .formParam("""introduced""", """2012-05-30""")
-      .formParam("""discontinued""", """""")
-      .formParam("""company""", """37"""))
-  */
+ val getUsers = scenario("Get Users Scenario")
+   .exec(http("get users").get("/users").check(status.is(200)))
 
- val scn = scenario("Get Users") // A scenario is a chain of requests and pauses
- .exec(http("request_1")
-   .get("/users"))
+ val sendMessages = scenario("Send Messages Scenario")
+   .exec(http("create messages")
+     .post("/messages").check(status.is(200))
+     .body(StringBody(
+       """{ "text": "Hello Gatling!", "userId": "1" ,
+           | "city":"Toronto", "temperature": "20.0",
+           | "latitude": "43.7", "longitude": "-79.42"}""".stripMargin)).asJSON)
 
-setUp(scn.inject(atOnceUsers(1)).protocols(httpConf))
+  val getMessages = scenario("Get Messages Scenario")
+    .exec(http("get all messages").get("/messages").check(status.is(200)))
+
+
+ setUp(getUsers.inject(rampUsers(100) over (10 seconds)).protocols(httpConf),
+       sendMessages.inject(rampUsers(100) over (10 seconds)).protocols(httpConf),
+       getMessages.inject(rampUsers(10) over (10 seconds)).protocols(httpConf))
+  .assertions(global.responseTime.percentile1.lt(400))
 }
